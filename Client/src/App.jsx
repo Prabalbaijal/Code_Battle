b57e870f0bcc4589be8674cd2907c9d3b4d334ef
Prabal Baijal <prabalbaijal11@gmail.com>
2024-10-30 17:46:05 +0530

    Login UI connected to Backend...

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import Login from './Components/Login/Login'
+import Login from './Components/Login/Login.jsx'
+import './index.css'
+import Dashboard from './Components/Dashboard.jsx'
+import {RouterProvider,createBrowserRouter} from 'react-router-dom'
+
+const router=createBrowserRouter([
+  {
+      path:"/",
+      element:<Login/>
+  },
+  {
+      path:"/dashboard",
+      element:<Dashboard/>
+  }
+]
+)
 
 export default function App() {
   return (
     <div>
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './Components/Login/Login.jsx'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store, { persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Toaster/>
-   <App></App>
-   <Login></Login>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster />
+        <App></App>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
