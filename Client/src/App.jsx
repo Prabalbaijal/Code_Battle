966ceb6ea89041b5061cae5fe732211336b980e9
Aashu2023 <aashishfeb2001@gmail.com>
2024-10-30 17:37:55 +0530

    Homepage edited

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import Login from './Components/Login/Login'
+import Login from './Components/Login/Login'
 import HomePage from './Components/HomePage/HomePage'
 export default function App() {
   return (
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-//import Login from './Components/Login/Login.jsx'
 import HomePage from './Components/HomePage/HomePage'
+import Login from './Components/Login/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <App></App>
-   
+   <Login></Login>
    <HomePage></HomePage>
   </StrictMode>,
 )
